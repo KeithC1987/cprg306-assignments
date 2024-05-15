de9393b217f52c1097dab0dfc0a8d2e44f6124c4
KeithC1987 <112564713+KeithC1987@users.noreply.github.com>
2024-05-15 01:22:09 -0600

    update all requirements of assignment 2
    create the week-2 file and add component student-info and set the week-2 link on the root page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+
+
+  const LinkStyle ="underline text-cyan-600 hover:text-cyan-300"
+
   return (
-    <main>
-      <h1>Hello guys!</h1>
+    <main className="h-screen">
+      <h1 className="text-2xl">CPRG 306 Web Development 2 - Assignments</h1>
+      <ul>
+        <li><Link className={LinkStyle} href="./week-2">week-2</Link></li>
+      </ul>
     </main>
   );
 }
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,4 +1,12 @@
-export default function StudentInfo(){
+import Link from "next/link";
 
-    
+export default function StudentInfo(){
+    return(
+       
+        <div>
+            <h1>My name:Yiu Kei Chan</h1>
+            <Link href="https://github.com/KeithC1987/cprg306-assignments.git">My GitHub repository</Link>
+        </div>
+
+    );
 }
